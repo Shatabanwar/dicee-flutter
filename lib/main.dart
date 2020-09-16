c63f3bc7dafb33eb0516df1f626d427ab21f96f3
Shatab Anwar <shatabanwar11@gmail.com>
2020-09-16 21:50:35 +0530

    Added buttons and functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,22 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+
+class _State extends State<DicePage> {
+
+  void updateDiceNumber(){
+
+    setState(() {
+      leftDiceNumber = Random.secure().nextInt(6) + 1;
+      rightDiceNumber = Random.secure().nextInt(6)+1;
+    });
+
+  }
+
+
+  static int leftDiceNumber = 1;
+  static int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed:(){
+                updateDiceNumber();
+              },
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: (){
+                updateDiceNumber();
+              },
             ),
           )
         ],
